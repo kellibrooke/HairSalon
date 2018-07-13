664fad112cfb7f6a050f44d397cfa2396bbab90c
Kelli <kellibrookelife@gmail.com>
2018-07-13 15:19:00 -0700

    create client views selectclient and index, adjust controllers to route correctly

--- HairSalon/Controllers/ClientController.cs
+++ HairSalon/Controllers/ClientController.cs
@@ -9,11 +9,31 @@ namespace HairSalon.Controllers
         [HttpPost("/clients")]
         public ActionResult ClientHome()
         {
-          string clientName = Request.Form["client-name"];
-          int clientStylistId = int.Parse(Request.Form["stylist-select"]);
-          Client newClient = new Client(clientName, clientStylistId);
-          newClient.SaveClient();
+            List<Stylist> stylistList = Stylist.GetAllStylists();
+            return View(stylistList);
         }
+        [HttpPost("/client-interaction")]
+        public ActionResult CreateClient()
+        {
+            string clientName = Request.Form["client-name"];
+            int clientStylistId = int.Parse(Request.Form["stylist-assign-select"]);
+            Client newClient = new Client(clientName, clientStylistId);
+            newClient.SaveClient();
+            List<Stylist> stylistList = Stylist.GetAllStylists();
+            return View("Index", stylistList);
+
+        }
+
+        [HttpPost("/select-client")]
+        public ActionResult SelectClient()
+        {
+            int stylistId = int.Parse(Request.Form["stylist-select"]);
+            Stylist selectedStylist = FindStylist(stylistId);
+            List<Client> selectedClientList = selectedStylist.GetClientList();
+            return View(selectedClientList);
+
+        }
+
         [HttpPost("/client-info")]
         public ActionResult ClientInfo()
         {
